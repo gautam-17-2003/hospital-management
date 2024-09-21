1ea2cd399f1768cd0002c4763815964434775e82
Gautam <159773892+gautam-17-2003@users.noreply.github.com>
2024-09-21 15:53:12 +0530

    Update MessageForm.jsx

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -15,7 +15,7 @@ const MessageForm = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/message/send",
+          `${process.env.BACKEND_URL}/api/v1/message/send`,
           { firstName, lastName, email, phone, message },
           {
             withCredentials: true,
