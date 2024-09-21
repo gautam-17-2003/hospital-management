19dcf43aaebc96d6b6f80f7997779a3336fc83f8
Gautam <159773892+gautam-17-2003@users.noreply.github.com>
2024-09-21 15:55:02 +0530

    Update AppointmentForm.jsx

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -27,7 +27,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/doctors",
+        `${process.env.BACKEND_URL}/api/v1/user/doctors`,
         { withCredentials: true }
       );
       setDoctors(data.doctors);
